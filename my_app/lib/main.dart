e1af9fae1f78ccf11cd1afbcf1ea68c1f7a3a632
koyu <mztnkoyu@g.ecc.u-tokyo.ac.jp>
2022-02-04 20:29:32 +0900

    jsonの保存がうまくいかないのでその前まで

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,30 +1,21 @@
 import 'package:flutter/material.dart';
 
-///////////////////////////////
-// ① Main：Flutterアプリもmain()からコードが実行されます。
-// `void main() => runApp(MyApp());` でも意味は同じです。
-void main() {
-  return runApp(MyApp());
-}
+// ①　必要なライブラリをインポート
+import 'package:shared_preferences/shared_preferences.dart';
 
 ///////////////////////////////
-// ② アプリの基盤：アプリのテーマやスタイルを設定する。その上のページを追加していく。
+void main() => runApp(MyApp());
+
+///////////////////////////////
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // MaterialAppという形式のアプリを作成
-    return MaterialApp(
-      theme: ThemeData(), // アプリのテーマカラーなど詳細を入力
-      home: MyHomePage(), // メインページを作成
-    );
+    return MaterialApp(theme: ThemeData(), home: MyHomePage());
   }
 }
 
 ///////////////////////////////
-// ⑦ MyHomePage本体
 class MyHomePage extends StatefulWidget {
-
-  // ⑦-1: TodoCardWidgetを一時的に保存する配列を作成
   List<Widget> cards = [];
 
   MyHomePage({Key? key}) : super(key: key);
@@ -33,42 +24,103 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-// ⑧ MyHomePageの状態
 class _MyHomePageState extends State<MyHomePage> {
+  // ② Widgetを初期化した際に呼ばれる`initState()`を上書き
+  @override
+  void initState() {
+    super.initState();
 
+    // ③ SharedPreferencesのインスタンスを取得し、
+    // SharedPreferencesに保存されているリストを取得
+    SharedPreferences.getInstance().then((prefs) {
+      var todo = prefs.getStringList("todo") ?? [];
+      for (var v in todo) {
+        setState(() {
+          // ④ 保存されている場合は、widgetのリストに追加
+          widget.cards.add(TodoCardWidget(label: v));
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Menu Bar"),
+        title: const Text("My TODO"),
+        actions: [
+          // ⑦ナビゲーションバーの右上にゴミ箱ボタンを設置
+          IconButton(
+              onPressed: () {
+                // ⑧ 押された場合は、保存されているTODOを全て削除
+                SharedPreferences.getInstance().then((prefs) async {
+                  await prefs.setStringList("todo", []);
+                  setState(() {
+                    widget.cards = [];
+                  });
+                });
+              },
+              icon: Icon(Icons.delete))
+        ],
       ),
       body: Center(
-        // ⑧-3: ListViewのchildrenをwidget.cardsに変更
-        child:  ListView.builder(
+        child: ListView.builder(
           itemCount: widget.cards.length,
           itemBuilder: (BuildContext context, int index) {
             return widget.cards[index];
           },
         ),
       ),
-      // ⑧-1: 画面右下にボタンを追加
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-          setState(() {
-            // ⑧-2: ボタンが押された時に、TodoCardWidgetをcardsに追加
-            widget.cards.add(TodoCardWidget(label: "a"));
-          });
+        onPressed: () async {
+          // ⑤ TextInputDialogを用いて、任意の文字列を取得する。
+          var label = await _showTextInputDialog(context);
+
+          if (label != null) {
+            setState(() {
+              widget.cards.add(TodoCardWidget(label: label));
+            });
+
+            // ⑥ SharedPreferencesのインスタンスを取得し、追加する。
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            var todo = prefs.getStringList("todo") ?? [];
+            todo.add(label);
+            await prefs.setStringList("todo", todo);
+          }
         },
         child: const Icon(Icons.add),
       ),
     );
   }
+
+  final _textFieldController = TextEditingController();
+
+  Future<String?> _showTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('TODO'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: const InputDecoration(hintText: "タスクの名称を入力してください。"),
+            ),
+            actions: <Widget>[
+              ElevatedButton(
+                child: const Text("キャンセル"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              ElevatedButton(
+                child: const Text('OK'),
+                onPressed: () => Navigator.pop(context, _textFieldController.text),
+              ),
+            ],
+          );
+        });
+  }
 }
 
 ////////////////////
-/// ⑤ StatefulWidget本体
 class TodoCardWidget extends StatefulWidget {
   final String label;
   var state = false;
@@ -79,7 +131,6 @@ class TodoCardWidget extends StatefulWidget {
   _TodoCardWidgetState createState() => _TodoCardWidgetState();
 }
 
-/// ⑤ TodoCardWidgetの状態
 class _TodoCardWidgetState extends State<TodoCardWidget> {
   void _changeState(value) {
     setState(() {
